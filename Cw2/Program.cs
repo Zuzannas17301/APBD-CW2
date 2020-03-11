8a11e081b3a6e585071a43a4e409bcfe1a01a460
zuzannas17301 <s17301@pjwstk.edu.pl>
2020-03-11 14:58:31 +0100

    dodane pliki

--- Cw2/Program.cs
+++ Cw2/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Cw2.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 
 namespace Cw2
 {
@@ -6,7 +10,38 @@ namespace Cw2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"Data\dane.csv";
+            Console.WriteLine("Hello World");
+
+            //Wczytywanie 
+            var fi = new FileInfo(path);
+            using (var stream = new StreamReader(fi.OpenRead()))
+            {
+                string line = null;
+                while ((line = stream.ReadLine()) != null)
+                {
+                    string[] kolumny = line.Split(',');
+                    Console.WriteLine(line);
+                }
+            }
+            //stream.Dispose();
+
+            //XML
+            var list = new List<Student>();
+            var st = new Student
+            {
+                Imie="Jan",
+                Nazwisko="Kowalski",
+                Email="kowalski@wp.pl"
+            };
+            list.Add(st);
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>),
+                new XmlRootAttribute("uczelnia"));
+            serializer.Serialize(writer, list);
+            serializer.Serialize(writer, list);
+
         }
     }
 }
